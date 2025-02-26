c47bb618278e8a05ae39eb58bdcf8f2e632b39d4
kohei126 <158240582+kohei126@users.noreply.github.com>
2025-02-26 10:53:13 +0900

    UnitBaseの変更

--- Src/Object/UnitBase.cpp
+++ Src/Object/UnitBase.cpp
@@ -1,5 +1,47 @@
+#include"../Application.h"
 #include "UnitBase.h"
+
+UnitBase::UnitBase()
+{
+	trans_.modelId = -1;
+	trans_.pos = { 0.0f,0.0f,0.0f };
+	trans_.scl = { 0.0f,0.0f,0.0f };
+	trans_.rot = { 0.0f,0.0f,0.0f };
+
+	anim_ = ANIM::NONE;
+	atcAnim_ = -1;
+	animTotalTime_ = -1;
+	stepAnim_ = -1.0f;
+	speedAnim_ = 1.0f;
+}
 
 UnitBase::~UnitBase()
 {
+}
+
+void UnitBase::ResetAnim(const ANIM _anim, const float _speed)
+{
+	
+}
+
+void UnitBase::Anim(void)
+{
+	// アニメーション再生
+	// 経過時間の取得
+	float deltaTime = 1.0f / Application::DEFAULT_FPS;
+	// アニメーション時間の進行
+	stepAnim_ += (speedAnim_ * deltaTime);
+	if (stepAnim_ > animTotalTime_)
+	{
+		//アニメーション終了時処理（継承先で行動を決めておく）
+		FinishAnim();
+	}
+	// 再生するアニメーション時間の設定
+	MV1SetAttachAnimTime(trans_.modelId, atcAnim_, stepAnim_);
+}
+
+void UnitBase::FinishAnim(void)
+{
+	//ループ再生
+	stepAnim_ = 0.0f;
 }
--- Src/Object/UnitBase.h
+++ Src/Object/UnitBase.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include<unordered_map>
 #include"Common/Transform.h"
 
 class UnitBase
@@ -7,11 +8,23 @@ class UnitBase
 public:
 
 	//***********************************************
+	//列挙型
+	//***********************************************
+
+	//アニメーション
+	enum class ANIM
+	{
+		NONE,	
+		IDLE,	//待機
+		WALK,	//歩き
+	};
+
+	//***********************************************
 	//メンバ関数
 	//***********************************************
 
 	//コンストラクタ
-	UnitBase() = default;
+	UnitBase();
 	//デストラクタ
 	virtual ~UnitBase() = 0;
 
@@ -24,6 +37,19 @@ public:
 	//解放
 	virtual void Release(void) = 0;
 
+	//位置
+	const VECTOR GetPos(void)const { return trans_.pos; };
+	//角度
+	const VECTOR GetRot(void)const { return trans_.rot; };
+	//大きさ
+	const VECTOR GetScl(void)const { return trans_.scl; };
+
+	//半径
+	const float GetRadius(void)const { return radius_; }
+
+	//アニメーションリセット
+	void ResetAnim(const ANIM _anim, const float _speed);
+
 protected:
 
 	//***********************************************
@@ -33,6 +59,25 @@ protected:
 	//モデルの情報
 	Transform trans_;
 
-	//
+	//半径
+	float radius_;
+
+	//アニメーション
+	ANIM anim_;								//アニメステート
+	std::unordered_map<ANIM, int> animNum_;	//アニメーションナンバー格納配列。
+	int atcAnim_;							//アタッチするアニメを格納
+	int animTotalTime_;						//アニメーションの総再生時間
+	float stepAnim_;						//アニメーションの再生時間
+	float speedAnim_;						//アニメーション速度
+
+
+	//***********************************************
+	//メンバ関数
+	//***********************************************
+
+	//アニメーション関数
+	void Anim(void);
+	//アニメーション終了時の動き
+	virtual void FinishAnim(void);
 };
 
