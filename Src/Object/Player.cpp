eb6918339911a97f1285379f50cdda31e4b5d921
DESKTOP-ROCE5BA\京佑 <otakukun0519@gmail.com>
2025-03-11 14:21:38 +0900

    プレイヤー座表移動設定

--- Src/Object/Player.cpp
+++ Src/Object/Player.cpp
@@ -1,3 +1,4 @@
+#include <DxLib.h>
 #include"../Utility/AsoUtility.h"
 #include "Player.h"
 
@@ -7,12 +8,34 @@ Player::~Player()
 
 void Player::Init(void)
 {
-	//trans_.pos = {0.0f,0.0f,0.0f};
+	trans_.pos = {0.0f,0.0f,0.0f};
 	trans_.pos = AsoUtility::VECTOR_ZERO;
 }
 
 void Player::Update(void)
 {
+
+	//à⁄ìÆ
+	 //ëO
+	if (CheckHitKey(KEY_INPUT_W))
+	{
+		trans_.pos.z += 1;
+	}
+	//ç∂
+	if (CheckHitKey(KEY_INPUT_A))
+	{
+		trans_.pos.x += 1;
+	}
+	//âE
+	if (CheckHitKey(KEY_INPUT_D))
+	{
+		trans_.pos.x += 1;
+	}
+	//å„ÇÎ
+	if (CheckHitKey(KEY_INPUT_S))
+	{
+		trans_.pos.z += 1;
+	}
 }
 
 void Player::Draw(void)
