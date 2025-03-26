995654530d2474c7d829e5eb08e4e6cfc90add05
eguma\riku <raito11092005@outlook.jp>
2025-03-26 12:49:31 +0900

    UI調整

--- Src/DrawUI/titleUiDraw.h
+++ Src/DrawUI/titleUiDraw.h
@@ -5,18 +5,10 @@ class StageUiFont;
 class titleUi
 {
 public:
-	enum class FONT_TYPE
-	{
-		STATE = 0,			//開始
-		OPERATION = 1,		//操作
-		EXPLANATION = 2,	//説明
-		CREDIT = 3,			//クレジット
-	};
 
 private:
 	int frameCount_;		       //フレームレート制御のための変数
 	const int blinkInterval_ = 30; //点滅間隔(フレーム数)
-	FONT_TYPE selectedFont_;       // 現在選択中のフォント
 	bool isBlinking_;			   //点滅フラグ
 	bool changeFlag_;       //切り替えフラグ
 
